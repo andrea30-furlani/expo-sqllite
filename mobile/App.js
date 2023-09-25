9b0adeaa0cf42e843c573b0380be459e780c2707
carloscastrofilho <cpcftq@gmail.com>
2023-09-25 15:48:51 -0300

    models/service-animal ok

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>projeto com  de dados</Text>
+      <Text>projeto de dados</Text>
       <StatusBar style="auto" />
     </View>
   );
